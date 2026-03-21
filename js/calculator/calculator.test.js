9ef598c335f565a2848fd20baa9a8d6ce0080f06
Ferrer-System <ortizdionicio993@gmail.com>
2026-03-20 18:16:13 -0600

    Test unitarios para userController y funciones

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -15,3 +15,24 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+// test de divide y multiply
+test('adds 10 / 2 to equal 5', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('adds 20 / 5 to equal 4', () => {
+  expect(calculator.divide(20, 4)).toBe(5);
+});
+
+test('subtract 5 * 4 to equal 10', () => {
+  expect(calculator.multiply(5, 4)).toBe(20);
+});
+
+test('subtract -5 * -10 to equal 50', () => {
+  expect(calculator.multiply(-5, -10)).toBe(50);
+});
+
+test('divide by zero ahould throw error', () => {
+  expect( () => calculator.divide(10, 0)).toThrow("Cannot divide by zero");
+});
+
--- js/user/user-controller.test.js
+++ js/user/user-controller.test.js
@@ -16,3 +16,47 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+//Agregar usuario que no estaba en la lista
+test('add user that is not in the list', () => {    
+    let user = new User(9999,"Ferrer", "ferrer@generation.org");
+    userController.add(user);    
+    expect(userController.getUsers()).toContain(user);
+  });
+
+
+// Usuario que no esta en la lista
+test('remove user that is not in the list', () => {    
+    let user = new User(8888,"Ghost", "ghost@generation.org");
+    let initialLength = userController.getUsers().length;
+    userController.remove(user);
+    expect(userController.getUsers().length).toBe(initialLength);
+  });
+
+  //caso exitoso
+test('find user by email not found', () => {    
+    let user = new User(1, "Anna", "anna@generation.org");
+    userController.add(user);    
+    let result = userController.findByEmail("anna@generation.org");
+    expect(result).toBe(user);
+  });
+
+  //Email no encontrado
+test('find user by email not found', () => {    
+    let result = userController.findByEmail("noexiste@generation.org");
+    expect(result).toBeUndefined();
+  });
+
+  //ID encontrado caso exitoso
+test('find user by id', () => {    
+    let user = new User(2, "Luis", "luis@generation.org");
+    userController.add(user);
+    let result = userController.findById(2);
+    expect(result).toBe(user);
+  });
+
+  //Id no encontrado
+test('find user by id not found', () => {    
+    let result = userController.findById(999);
+    expect(result).toBeUndefined();
+  });
+  
